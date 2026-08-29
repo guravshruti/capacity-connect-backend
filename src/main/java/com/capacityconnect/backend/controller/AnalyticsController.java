b778d37b823d6bc196d6ea03aacb543261b21c01
Suchita Patil <sp2167080@gmail.com>
2026-08-30 00:15:39 +0530

    Add analytics summary feature (total users, total courses, most popular course)

---
+++ src/main/java/com/capacityconnect/backend/controller/AnalyticsController.java
@@ -0,0 +1,21 @@
+package com.capacityconnect.backend.controller;
+
+import com.capacityconnect.backend.model.AnalyticsSummary;
+import com.capacityconnect.backend.service.AnalyticsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/analytics")
+public class AnalyticsController {
+
+    @Autowired
+    private AnalyticsService analyticsService;
+
+    @GetMapping("/summary")
+    public AnalyticsSummary getSummary() {
+        return analyticsService.getSummary();
+    }
+}
